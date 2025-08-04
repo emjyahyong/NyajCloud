aab7ff6e191769cff722b019e14af84075357bf7
Emjy <emjy.ahyong@gmail.com>
2025-08-04 17:16:40 +0400

    git init

---
+++ src/main/java/com/nyajcloud/NyajCloudApplication.java
@@ -0,0 +1,13 @@
+package com.nyajcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyajCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyajCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyajcloud/NyajCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyajcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyajCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
