5de86416a2e7476a093650c2ce135951e8b4ed04
Emjy <emjy.ahyong@gmail.com>
2025-08-13 17:18:12 +0400

    Suppression temporaire Spring Security pour faciliter les tests du CRUD avec succès

--- src/main/java/com/nyajcloud/repository/UserRepository.java
+++ src/main/java/com/nyajcloud/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.nyajcloud.repository;
 
 import com.nyajcloud.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "utilisateurs")
 public interface UserRepository extends JpaRepository<User, Long> {
 }
